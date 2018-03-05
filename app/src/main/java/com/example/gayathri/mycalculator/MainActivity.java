db4ed3c24e3beda7b100349e54c48d9a78997ad5
Gayathri R <iamanifs@gmail.com>
2018-03-05 18:30:53 +0530

    Working functionalities

--- app/src/main/java/com/example/gayathri/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/gayathri/mycalculator/MainActivity.java
@@ -7,13 +7,14 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private String a = "";
     private String b = "";
     private TextView t ;
-    private int op,k,c;
+    private int op,k = 0,c = 0;
     private int sem = 0;
 
     @Override
@@ -25,26 +26,29 @@ public class MainActivity extends AppCompatActivity {
 
     public void charToNumber(View v){
         Button btn = (Button)findViewById(v.getId());
-        if(sem == 0) {
-            a += btn.getText().toString();
-            t.setText(a);
-        }
-        else {
+        //if(sem == 0) {
+            String z = btn.getText().toString();
+            a += z;
+            t.setText(t.getText() + z);
+        /* {
             String ch = btn.getText().toString();
             b += btn.getText().toString();
             t.setText(t.getText() + ch);
-        }
+        }*/
     }
 
     public void set(View v){
         String q = t.getText().toString();
         t.setText(q + "=");
+        k++;
         performance();
+        t.setText(t.getText().toString() + c);
     }
     public void reset(View v){
         sem = 0;
         a = "";
         b = "";
+        c = 0;
         k = 0;
         t.setText("");
     }
@@ -52,6 +56,8 @@ public class MainActivity extends AppCompatActivity {
     public void operation(View v){
         Button btn = (Button)findViewById(v.getId());
         String x = btn.getText().toString();
+        /*Toast g = Toast.makeText(getApplicationContext(),x,Toast.LENGTH_LONG);
+        g.show();*/
         sem = 1;
         /*switch(x){
             case "+":
@@ -72,40 +78,51 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
         }*/
-        if(x == "+"){
+        if(x.equals("+")){
             op = 0;
             k++;
         }
-        else if(x == "-"){
+        else if(x.equals("-")){
+            /*Toast t = Toast.makeText(getApplicationContext(),"Sub",Toast.LENGTH_LONG);
+            t.show();*/
             op = 1;
             k++;
         }
-        else if(x == "*"){
+        else if(x.equals("*")){
+            /*Toast t = Toast.makeText(getApplicationContext(),"Sub",Toast.LENGTH_LONG);
+            t.show();*/
             op = 2;
             k++;
         }
-        else if(x == "/"){
+        else if(x.equals("/")){
+            /*Toast t = Toast.makeText(getApplicationContext(),"Sub",Toast.LENGTH_LONG);
+            t.show();*/
+
             op = 3;
             k++;
         }
         t.setText(t.getText() + x);
+        performance();
 
     }
     public void performance() {
-        int n1;
-        int n2;
+        //int n1;
+        //int n2;
         switch(op){
             case 0:
                 try{
-                    n1 = Integer.parseInt(a);
-                    n2 = Integer.parseInt(b);
-                    if(k >= 1){
-                        n1 = c;
-                        b = "";
+                    /*n1 = Integer.parseInt(a);
+                    n2 = Integer.parseInt(b);*/
+                    if(k == 1){
+                        c = Integer.parseInt(a);
+                        //b = "";
                     }
-                    c = n1 + n2;
-                    String q = t.getText().toString();
-                    t.setText(q + c);
+                    else {
+                        c += Integer.parseInt(a);
+                    }
+                    a = "";
+                    //String q = t.getText().toString();
+                    //t.setText(q + c);
                 }
                 catch(Exception e) {
                     t.setText("Error");
@@ -113,15 +130,18 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case 1:
                 try{
-                    n1 = Integer.parseInt(a);
-                    n2 = Integer.parseInt(b);
-                    if(k >= 1){
-                        n1 = c;
-                        b = "";
+                    /*n1 = Integer.parseInt(a);
+                    n2 = Integer.parseInt(b);*/
+                    if(k == 1){
+                        c = Integer.parseInt(a);
+                        //b = "";
                     }
-                    c = n1 - n2;
-                    String q = t.getText().toString();
-                    t.setText(q + c);
+                    else {
+                        c -= Integer.parseInt(a);
+                    }
+                    /*String q = t.getText().toString();
+                    t.setText(q + c);*/
+                    a = "";
                 }
                 catch(Exception e) {
                     t.setText("Error");
@@ -129,15 +149,16 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case 2:
                 try{
-                    n1 = Integer.parseInt(a);
-                    n2 = Integer.parseInt(b);
-                    if(k >= 1){
-                        n1 = c;
-                        b = "";
+                    /*n1 = Integer.parseInt(a);
+                    n2 = Integer.parseInt(b);*/
+                    if(k == 1){
+                        c = Integer.parseInt(a);
                     }
-                    c = n1 * n2;
-                    String q = t.getText().toString();
-                    t.setText(q + c);
+                    else
+                    c *= Integer.parseInt(a);
+                    a = "";
+                    /*String q = t.getText().toString();
+                    t.setText(q + c);*/
                 }
                 catch(Exception e) {
                     t.setText("Error");
@@ -145,15 +166,16 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case 3:
                 try{
-                    n1 = Integer.parseInt(a);
-                    n2 = Integer.parseInt(b);
-                    if(k >= 1){
-                        n1 = c;
-                        b = "";
+                    /*n1 = Integer.parseInt(a);
+                    n2 = Integer.parseInt(b);*/
+                    if(k == 1){
+                        c = Integer.parseInt(a);
+
                     }
-                    c = n1 / n2;
-                    String q = t.getText().toString();
-                    t.setText(q + c);
+                    else
+                        c /= Integer.parseInt(a) ;
+                    /*String q = t.getText().toString();
+                    t.setText(q + c);*/
                 }
                 catch(Exception e) {
                     t.setText("Error");
